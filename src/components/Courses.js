f2bda0c201be3654669bfeb21dc91f157188c2af
Mitch-Tyrer <mltcinematography@gmail.com>
2019-02-27 09:49:05 -0700

    refactored course data to be single component

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -4,12 +4,10 @@ import {
   NavLink,
   Redirect
 } from 'react-router-dom';
+import { HTMLCourses, CSSCourses, JSCourses } from '../data/courses';
 
 //course componenets
-import Course from './courses/Course';
-import CSS from "./courses/CSS";
-import HTML from './courses/HTML';
-import JavaScript from './courses/JavaScript';
+import CourseContainer from './courses/CourseContainer'
 
 const Courses = ({match}) => (
   <div className="main-content courses">
@@ -24,9 +22,9 @@ const Courses = ({match}) => (
 
     {/* Write routes here... */}
       <Route exact path={match.path} render={ () => <Redirect to={`${match.path}/html`} /> } />
-      <Route path={`${match.path}/html`} component={HTML} />
-      <Route path={`${match.path}/css`} component={CSS} />
-      <Route path={`${match.path}/javascript`} component={JavaScript} />
+      <Route path={`${match.path}/html`} render={() => <CourseContainer data={HTMLCourses} />} />
+      <Route path={`${match.path}/css`} render={() => <CourseContainer data={CSSCourses} />} />
+      <Route path={`${match.path}/javascript`} render={() => <CourseContainer data={JSCourses} />} />
   </div>
 );
 
