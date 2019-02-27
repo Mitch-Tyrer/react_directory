75019befafd972911024dc66734df2c3d9222ebf
Mitch-Tyrer <mltcinematography@gmail.com>
2019-02-27 10:18:21 -0700

    added dynamically created links to home page

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
-const Featured = () => {
+const Featured = ({match}) => {
+  let fname = `${match.params.fname}`;
+  let lname = `${match.params.lname}`;
+  let topic = match.params.topic;
+  let name;
+  if (lname === 'undefined'){
+   name = `${fname}`;
+  } else {
+   name = `${fname} ${lname}`
+  } 
 
   return (
     <div className="main-content">
-      <h2>Featured: </h2>
-      <p>Introducing <strong></strong>, a teacher who loves teaching courses about <strong></strong>!</p>
+      <h2>{name}</h2>
+      <p>Introducing <strong>{name}</strong>, a teacher who loves teaching courses about <strong>{topic}</strong>!</p>
     </div>
   );
 }
